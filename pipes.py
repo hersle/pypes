f0d8a47caf1b0c479c40671191d8e31eaf0ffc88
Herman <hermansletmoen@gmail.com>
2014-07-17 23:47:14 +0200

    Minor refactoring

--- pipes.py
+++ pipes.py
@@ -13,12 +13,6 @@ CELL_PIPE_ACTIVE = 3    # identifies a cell with the currently selected pipe
 CELL_START = 4          # identifies a cell where the water flow starts
 CELL_FINISH = 5         # identifies a cell where the water flow should end
 
-PIPE_CLOSED = (
-    ((1, 0), (1, 1)),
-    ((2, 1), (1, 1)),
-    ((1, 2), (1, 1)),
-    ((0, 1), (1, 1)),
-)
 PIPE_STRAIGHT = (
     ((0, 1), (1, 1), (2, 1)),
     ((1, 0), (1, 1), (1, 2))
@@ -38,24 +32,29 @@ PIPE_T = (
 PIPE_X = (
     ((1, 0), (0, 1), (1, 1), (2, 1), (1, 2)),
 )
+PIPE_CLOSED = (
+    ((1, 0), (1, 1)),
+    ((2, 1), (1, 1)),
+    ((1, 2), (1, 1)),
+    ((0, 1), (1, 1)),
+)
 PIPES = (PIPE_STRAIGHT, PIPE_TURN, PIPE_T, PIPE_X, PIPE_CLOSED)
 
 def place_pipe(pipe, pipe_x, pipe_y, pipe_r, board):
-    for row in board[pipe_y : pipe_y + 3]:
-        for cell in row[pipe_x : pipe_x + 3]:
-            if cell != CELL_EMPTY:
-                return # refuse placement if tile occupied
-    for x, y in pipe[pipe_r]:
-        board[pipe_y + y][pipe_x + x] = CELL_PIPE
+    # Only place pipe if the tile is free.
+    # The tile is free if its center cell is empty, 
+    # since all pipes use a tile's center cell.
+    if board[pipe_y + 1][pipe_x + 1] == CELL_EMPTY:
+        for x, y in pipe[pipe_r]:
+            board[pipe_y + y][pipe_x + x] = CELL_PIPE
 
 def flow_water(board, flow_endpoints):
     new_flow_endpoints = []
     for x, y in flow_endpoints:
         moved_endpoint = False
-        # Flow water into any perpendicular adjacent pipes
+        # Flow water into any adjacent pipes
         for dx, dy in ((0, -1), (1, 0), (0, 1), (-1, 0)): 
             if board[y + dy][x + dx] not in (CELL_EMPTY, CELL_PIPE_WATER):
-                log("flowing from (%d, %d) to (%d, %d)" % (x, y, x + dx, y + dy))
                 board[y + dy][x + dx] = CELL_PIPE_WATER
                 new_flow_endpoints.append((x + dx, y + dy))
                 moved_endpoint = True
@@ -70,28 +69,29 @@ def print_board(win, board, pipe, pipe_x, pipe_y, pipe_r):
             if pipe and (x - pipe_x, y - pipe_y) in pipe[pipe_r]:
                 win.addstr("  ", curses.color_pair(CELL_PIPE_ACTIVE))
             elif board[y][x] == CELL_EMPTY:
-                win.addstr("░░")
+                win.addstr("  ")
             else:
                 win.addstr("  ", curses.color_pair(board[y][x]))
     win.refresh()
 
 def load_board(level):
-    global BOARD_HEIGHT, BOARD_WIDTH
+    global BOARD_HEIGHT, BOARD_WIDTH # modify global variables, not local ones
     board = []
-    start_x, start_y, finish_x, finish_y = None, None, None, None
+    start_x, start_y, finish_x, finish_y = None, None, None, None # TODO: make global for consistency?
     lines = open("levels/level%d" % level).read().splitlines()
     for y in range(0, len(lines)):
-        BOARD_HEIGHT += 1
+        BOARD_HEIGHT += 1 # board height increases by 1 for each line
         row = []
         for x in range(0, len(lines[y])):
-            BOARD_WIDTH = x + 1 if x + 1 > BOARD_WIDTH else BOARD_WIDTH
+            if x + 1 > BOARD_WIDTH: # increase board width if exceeded
+                BOARD_WIDTH = x + 1
             char = lines[y][x]
             if char == ".":
                 row.append(CELL_EMPTY)
             elif char == "S" or char == "s":
+                row.append(CELL_START)
                 if char == "S": # cap S denotes flow starting point
                     start_x, start_y = x, y
-                row.append(CELL_START)
             elif char == "F" or char == "f": # cap F denotes flow ending point
                 row.append(CELL_FINISH)
                 if char == "F":
@@ -107,18 +107,17 @@ def init_colors():
     curses.init_pair(CELL_FINISH, curses.COLOR_BLACK, curses.COLOR_RED)
 
 def main(screen):
-    curses.curs_set(False) # disable cursor
+    curses.curs_set(False) # hide cursor from terminal
     init_colors()
 
     board, start_x, start_y, finish_x, finish_y = load_board(1)
+    flow_endpoints = [(start_x, start_y)]
     board_win = curses.newwin(BOARD_HEIGHT + 2, BOARD_WIDTH * 2 + 2)
     board_win.keypad(True) # interpret special key presses (e.g. arrow keys)
     board_win.border()
 
-    log("Start=(%d, %d), finish=(%d, %d)" % (start_x, start_y, finish_x, finish_y))
-    flow_endpoints = [(start_x, start_y)]
     pipe = None
-    x, y, r = 0, 0, 0
+    x, y, r = 0, 0, 0 # position from right, position from left, steps rotated
     while True:
         print_board(board_win, board, pipe, x, y, r)
 
