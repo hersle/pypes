5f728fa37fd4c7a629c2829468cbe9bf717015d2
Herman <hermansletmoen@gmail.com>
2014-07-28 23:16:34 +0200

    Added classes and did major clean-up

--- game.py
+++ game.py
@@ -1,112 +1,141 @@
 import curses
 import pipes
 import menu
-import levelio
 from time import time
 import log
 
-TILE_SIZE = 3
 
-def start_flow(level):
-    for start_x, start_y in level["starts"]:
-        level["board"][start_y][start_x] = pipes.CELL_PIPE_WET
-        level["flow"].append((start_x, start_y))
-    level["flow_started"] = True
+class Level:
 
-def flow_water(level):
-    if not level["flow_started"]:
-        start_flow(level)
-    else:  # expand flow
-        new_flow = []
-        for x, y in level["flow"]:
+    TILE_SIZE = 3
 
-            if is_at_board_edge(level["board"], x, y):
-                level["lost"] = True  # game lost if flow is at board edge
-                return
+    def __init__(self, level_number):
+        self.load_level(level_number)
+        self.lost = False
+        self.won = False
 
-            offx = -1 + x % TILE_SIZE  # cells left/right of tile center
-            offy = -1 + y % TILE_SIZE  # cells above/below tile center
+    def load_level(self, level_number):
+        level_file = open("levels/" + str(level_number))
+        lines = level_file.read().splitlines()
+        level_file.close()
+        lines_pipes = [line for line in lines if "=" in line]
+        lines_board = [line for line in lines if "=" not in line]
 
-            # Check cells   above    right   below   left
+        self.load_pipes(lines_pipes)
+        starts, checkpoints, finishes = self.load_board(lines_board)
+        self.flow = Flow(starts, checkpoints, finishes)
+
+    def load_pipes(self, lines_pipes):
+        pipe_quantities = dict(line.split("=") for line in lines_pipes)
+        for pipe in pipes.pipe_list:
+            # Set pipe quantity as specified or 0 if unspecified
+            pipe.quantity = int(pipe_quantities.get(pipe.char, 0))
+        self.pipes = pipes.pipe_list
+
+    def load_board(self, lines_board):
+        self.board = []
+        starts = []
+        checkpoints = []
+        finishes = []
+
+        for y, line in enumerate(lines_board):
+            row = []
+            for x, char in enumerate(line):
+                cell = pipes.CELL_MAP[char]
+                row.append(cell)
+                if cell == pipes.CELL_START:
+                    starts.append((x, y))
+                elif cell == pipes.CELL_CHECKPOINT:
+                    checkpoints.append((x, y))
+                elif cell == pipes.CELL_FINISH:
+                    finishes.append((x, y))
+            self.board.append(row)
+
+        self.height = len(self.board)
+        self.width = max(len(row) for row in self.board)
+        return starts, checkpoints, finishes
+
+    def select_pipe(self, number, current_pipe):
+        index = number - 1  # pressing 1 should select pipe at index 0, etc
+        if index in range(0, len(self.pipes)):
+            return self.pipes[index]
+        return current_pipe
+
+    def advance_flow(self):
+        return_code = self.flow.advance(self.board)
+        self.lost = return_code == self.flow.RETURN_CODE_GAME_LOST
+        self.won = return_code == self.flow.RETURN_CODE_GAME_WON
+
+class Flow:
+
+    # Returned by advance() to inform its caller if the game is lost or won
+    RETURN_CODE_GAME_LOST = 0
+    RETURN_CODE_GAME_WON = 1
+
+    def __init__(self, starts, checkpoints, finishes):
+        self.starts = starts
+        self.checkpoints = checkpoints
+        self.finishes = finishes
+        self.started = False
+
+    def start(self, board):
+        self.endpoints = []
+        for start_x, start_y in self.starts:
+            board[start_y][start_x] = pipes.CELL_PIPE_WET
+            self.endpoints.append((start_x, start_y))
+        self.started = True
+        self.targets_wet = 0
+
+    def advance(self, board):
+        if not self.started:
+            self.start(board)
+        else:
+            new_endpoints = []
+            for x, y in self.endpoints:
+                # Game over if endpoint is at board edge
+                if y in (0, len(board) - 1) or x in (0, len(board[y]) - 1):
+                    return self.RETURN_CODE_GAME_LOST
+                offx, offy = -1 + x % 3, -1 + y % 3  # offset from tile center
+                for dx, dy in ((0, -1), (1, 0), (0, 1), (-1, 0)):
+                    cell = board[y + dy][x + dx]
+                    if cell != pipes.CELL_PIPE_WET:  # do not wet a cell twice
+                        if cell != pipes.CELL_EMPTY:  # i.e. is a pipe
+                            board[y + dy][x + dx] = pipes.CELL_PIPE_WET
+                            if not self.is_dead_end(board, x + dx, y + dy):
+                                new_endpoints.append((x + dx, y + dy))
+                        elif (dx, dy) == (offx, offy):
+                            # Lost if attempting to fill a cell in the same 
+                            # direction from the tile center as the direction
+                            # this endpoint itself is from tile center.
+                            return self.RETURN_CODE_GAME_LOST
+            self.endpoints = new_endpoints
+            log.log("endpoints: %s" % str(self.endpoints))
+
+            # Check whether game is lost or won if the flow has stopped
+            if len(self.endpoints) == 0:
+                # Won if all checkpoints and finishes are wet, otherwise lost
+                for x, y in self.checkpoints + self.finishes:
+                    if board[y][x] != pipes.CELL_PIPE_WET:
+                        return self.RETURN_CODE_GAME_LOST
+                return self.RETURN_CODE_GAME_WON
+
+    def is_dead_end(self, board, x, y):
+        offx = -1 + x % 3
+        offy = -1 + y % 3
+        if offx == 0 and offy == 0:
             for dx, dy in ((0, -1), (1, 0), (0, 1), (-1, 0)):
-                cell = level["board"][y + dy][x + dx]
-                if cell != pipes.CELL_PIPE_WET:  # do not wet a pipe twice
-                    if cell != pipes.CELL_EMPTY:
-                        level["board"][y + dy][x + dx] = pipes.CELL_PIPE_WET
-                        new_flow.append((x + dx, y + dy))
-                    elif (dx, dy) == (offx, offy): #and cell == pipes.CELL_EMPTY:
-                        level["lost"] = True
-                        return
+                if board[y + dy][x + dx] in pipes.CELLS_FILLABLE:
+                    return False
+            return True
+        return False
 
-        #level["won"] = game_is_won(level, new_flow)
-        goals_wet = 0  # wet finishes and checkpoints
-        goals =  level["finishes"] + level["checkpoints"]
-        for x, y in goals:
-            if level["board"][y][x] == pipes.CELL_PIPE_WET:
-                goals_wet += 1
-        all_goals_wet = goals_wet == len(goals)
-        log.log("%d of %d goals covered; all_goals_wet=%s" % (goals_wet, len(goals), all_goals_wet))
-
-        dead_ends = 0
-        for x, y in new_flow:
-            if is_dead_end(level["board"], x, y):
-                dead_ends += 1
-        all_ends_dead = dead_ends == len(new_flow)
-        log.log("%d of %d ends dead; all_ends_dead=%s\n" % (dead_ends, len(new_flow), all_ends_dead))
-
-        if all_ends_dead:
-            if all_goals_wet:
-                level["won"] = True
-                return
-            else:
-                level["lost"] = True
-                return
-
-        #level["won"] = goals_covered
-        level["flow"] = new_flow
-
-def is_at_board_edge(board, x, y):
-    return y in (0, len(board) - 1) or x in (0, len(board[y]) - 1)
-
-def is_dead_end(board, x, y):
-    offx = -1 + x % TILE_SIZE  # cells left/right of tile center
-    offy = -1 + y % TILE_SIZE  # cells above/below tile center
-    if offx == 0 and offy == 0:  # must be center in tile
-        for dx, dy in ((0, -1), (1, 0), (0, 1), (-1, 0)):
-            if board[y+dy][x+dx] not in (pipes.CELL_PIPE_WET, pipes.CELL_EMPTY):
-                return False
-        return True
-    return False
-
-def game_is_won(level, flow_endpoints):
-    # TODO: fix game is won
-    for x, y in level["finishes"] + level["checkpoints"]:
-        if level["board"][y][x] != pipes.CELL_PIPE_WET:
-            return False
-    for x, y in flow_endpoints:
-        if not is_dead_end(level["board"], x, y):
-            return False
-    return True
-
-def place_pipe(pipe, pipe_x, pipe_y, pipe_r, board):
-    # Place pipe if tile's center cell is free
-    if board[pipe_y + 1][pipe_x + 1] == pipes.CELL_EMPTY:
-        for x, y in pipe["coords"][pipe_r]:
-            board[pipe_y + y][pipe_x + x] = pipes.CELL_PIPE_DRY
-        pipe["qty"] -= 1
-
-
-def select_pipe(number, pipelist, current_pipe):
-    index = number - 1
-    if index in range(0, len(pipelist)) and pipelist[index]["qty"] > 0:
-        return pipelist[index]
-    return current_pipe  # return current pipe if selection denied
+# End of classes
 
 def print_board(win, board, pipe, pipe_x, pipe_y, pipe_r):
     for y in range(0, len(board)):
         win.move(1 + y, 1)  # +1 to compensate for window border
         for x in range(0, len(board[y])):
-            if pipe and (x - pipe_x, y - pipe_y) in pipe["coords"][pipe_r]:
+            if pipe and (x - pipe_x, y - pipe_y) in pipe.coordinates[pipe_r]:
                 win.addstr("██", curses.color_pair(pipes.CELL_PIPE_ACTIVE))
             elif board[y][x] == pipes.CELL_EMPTY:
                 win.addstr("  ")
@@ -119,10 +148,10 @@ def print_pipes(win, pipelist):
     for p, pipe in enumerate(pipelist):
         base_x = 1 + p * 12
         win.addstr(1, base_x, "[%d]" % (p + 1))
-        win.addstr(2, base_x, "%02dx" % pipe["qty"])
+        win.addstr(2, base_x, "%02dx" % pipe.quantity)
         base_x += 4
-        for x, y in pipe["coords"][0]:
-            string = "░░" if pipe["qty"] == 0 else "██"
+        for x, y in pipe.coordinates[0]:
+            string = "░░" if pipe.quantity == 0 else "██"
             win.addstr(1 + y, base_x + x * 2, string, curses.color_pair(pipes.CELL_PIPE_DRY))
     win.refresh()
 
@@ -173,51 +202,53 @@ def play(screen, level_number):
     controls = {}
     exec(open("controls.py").read(), controls)
 
-    level = levelio.load_level(level_number)
-    board_width = len(level["board"][0])  # assume board is rectangular
-    board_height = len(level["board"])
+    level = Level(level_number)
 
-    pipe_win, board_win, misc_win = init_windows(screen, board_width, board_height)
+    pipe_win, board_win, misc_win = init_windows(screen, level.width, level.height)
 
-    pipe = next(p for p in level["pipes"] if p["qty"] > 0)  # current pipe
+    # Select first pipe with a quantity above 0
+    pipe = next(pipe for pipe in level.pipes if pipe.quantity > 0)  # current pipe
     x = 0  # blocks from board's left
     y = 0  # blocks from board's top
     r = 0  # steps rotated
-    flow_speed = 0.5  # seconds till flow flows
+    flow_speed = 0.5  # seconds between each time flow advances
     flow_start_delay = 2  # seconds till flow starts
     flow_time = time() + flow_start_delay  # timestamp at which flow flows
     while True:
+        # Timeout when flow should advance
         board_win.timeout(int((flow_time - time()) * 1000))  # s to ms
 
-        print_board(board_win, level["board"], pipe, x, y, r)
-        print_pipes(pipe_win, level["pipes"])
+        print_board(board_win, level.board, pipe, x, y, r)
+        print_pipes(pipe_win, level.pipes)
         #print_misc(misc_win)
 
         ch = board_win.getch() # get key press
         # Order of operations below does matter
         if ch != -1 and chr(ch).isdigit():  # select new pipe
-            pipe = select_pipe(int(chr(ch)), level["pipes"], pipe)
+            pipe = level.select_pipe(int(chr(ch)), pipe)
             r = 0  # reset rotation
         elif ch in controls["place_pipe"] and pipe:
-            place_pipe(pipe, x, y, r, level["board"])
-            if pipe["qty"] == 0: pipe = None  # deselect if empty
+            pipe.place(x, y, r, level.board)
+            if pipe.quantity == 0:
+                pipe = None  # deselect pipe if depleted
         elif ch in controls["move_up"]:
-            y = max(0, y - TILE_SIZE)
+            y = max(0, y - level.TILE_SIZE)
         elif ch in controls["move_right"]:
-            x = min(board_width - TILE_SIZE, x + TILE_SIZE)
+            x = min(level.width - level.TILE_SIZE, x + level.TILE_SIZE)
         elif ch in controls["move_down"]:
-            y = min(board_height - TILE_SIZE, y + TILE_SIZE)
+            y = min(level.height - level.TILE_SIZE, y + level.TILE_SIZE)
         elif ch in controls["move_left"]:
-            x = max(0, x - TILE_SIZE)
+            x = max(0, x - level.TILE_SIZE)
         elif ch in controls["rotate"] and pipe:
-            r = (r + 1) % len(pipe["coords"])
-        #elif ch in controls["increase_flow_speed"]:
-            #flow_speed /= 10
+            r = (r + 1) % len(pipe.coordinates)
+        elif ch in controls["increase_flow_speed"]:
+            flow_speed /= 10
         elif ch == ord("q"):
             return
-        elif ch == ord("f"):
-            flow_water(level)
+        elif ch == -1: #ord("f"):
+            level.advance_flow()
             flow_time = time() + flow_speed  # update flow time
-        if level["won"] or level["lost"]:
-            on_game_end(board_win, level["won"], level_number)
+
+        if level.won or level.lost:
+            on_game_end(board_win, level.won, level_number)
             return
--- pipes.py
+++ pipes.py
@@ -6,64 +6,53 @@ CELL_PIPE_ACTIVE = 3
 CELL_START = 4
 CELL_CHECKPOINT = 5
 CELL_FINISH = 6
+CELLS_FILLABLE = (CELL_PIPE_DRY, CELL_START, CELL_CHECKPOINT, CELL_FINISH)
 
-CELL_MAP = (
-    ".", CELL_EMPTY,
-    "p", CELL_PIPE_DRY,
-    "s", CELL_START,
-    "c", CELL_CHECKPOINT,
-    "f", CELL_FINISH
-)
+# Characters associated with each cell. Used when reading levels from files.
+CELL_MAP = {
+    ".": CELL_EMPTY,
+    "p": CELL_PIPE_DRY,
+    "s": CELL_START,
+    "c": CELL_CHECKPOINT,
+    "f": CELL_FINISH
+}
 
 # Pipes
-PIPE_STRAIGHT = {
-    "id" : 0,
-    "char" : "I",
-    "qty" : 0,
-    "coords" : (
-        ((0, 1), (1, 1), (2, 1)),
-        ((1, 0), (1, 1), (1, 2))
-    )
-}
-PIPE_TURN = {
-    "id" : 1,
-    "char" : "L",
-    "qty" : 0,
-    "coords" : (
-        ((1, 0), (1, 1), (2, 1)),
-        ((2, 1), (1, 1), (1, 2)),
-        ((0, 1), (1, 1), (1, 2)),
-        ((1, 0), (1, 1), (0, 1))
-    )
-}
-PIPE_T = {
-    "id" : 2,
-    "char" : "T",
-    "qty" : 0,
-    "coords" : (
-        ((1, 0), (0, 1), (1, 1), (2, 1)),
-        ((1, 0), (1, 1), (2, 1), (1, 2)),
-        ((0, 1), (1, 1), (2, 1), (1, 2)),
-        ((1, 0), (0, 1), (1, 1), (1, 2))
-    )
-}
-PIPE_X = {
-    "id" : 3,
-    "char" : "X",
-    "qty" : 0,
-    "coords" : (
-        ((1, 0), (0, 1), (1, 1), (2, 1), (1, 2)),
-    )
-}
-PIPE_CLOSED = {
-    "id" : 4,
-    "char" : "i",
-    "qty" : 0,
-    "coords" : (
-        ((1, 0), (1, 1)),
-        ((2, 1), (1, 1)),
-        ((1, 2), (1, 1)),
-        ((0, 1), (1, 1)),
-    )
-}
-PIPES = (PIPE_STRAIGHT, PIPE_TURN, PIPE_T, PIPE_X, PIPE_CLOSED)
+class Pipe:
+    def __init__(self, _id, char, coordinates):
+        self._id = _id
+        self.char = char
+        self.coordinates = coordinates
+
+    def place(self, board_x, board_y, r, board):
+        if board[board_y + 1][board_x + 1] == CELL_EMPTY:
+            for x, y in self.coordinates[r]:
+                board[board_y + y][board_x + x] = CELL_PIPE_DRY
+            self.quantity -= 1
+
+PIPE_STRAIGHT = Pipe(0, "I", (
+    ((0, 1), (1, 1), (2, 1)),
+    ((1, 0), (1, 1), (1, 2))
+))
+PIPE_TURN = Pipe(1, "L", (
+    ((1, 0), (1, 1), (2, 1)),
+    ((2, 1), (1, 1), (1, 2)),
+    ((0, 1), (1, 1), (1, 2)),
+    ((1, 0), (1, 1), (0, 1))
+))
+PIPE_T = Pipe(2, "T", (
+    ((1, 0), (0, 1), (1, 1), (2, 1)),
+    ((1, 0), (1, 1), (2, 1), (1, 2)),
+    ((0, 1), (1, 1), (2, 1), (1, 2)),
+    ((1, 0), (0, 1), (1, 1), (1, 2))
+))
+PIPE_X = Pipe(3, "X", (
+    ((1, 0), (0, 1), (1, 1), (2, 1), (1, 2)),
+))
+PIPE_CLOSED = Pipe(4, "i", (
+    ((1, 0), (1, 1)),
+    ((2, 1), (1, 1)),
+    ((1, 2), (1, 1)),
+    ((0, 1), (1, 1)),
+))
+pipe_list = (PIPE_STRAIGHT, PIPE_TURN, PIPE_T, PIPE_X, PIPE_CLOSED)
